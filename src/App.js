c98cc079868fd1926fa9ce111a87942558a034f3
Jon O'Drobinak <jodrobinak@gmail.com>
2019-08-20 15:47:07 -0600

    Add crawl, CardContainer components
    co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
-import Crawl from 'react-star-wars-crawl';
+import Crawl from './Crawl';
+import CardContainer from './CardContainer';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+    filmData: [],
+    peopleData: [],
+    planetData: [],
+    vehicleData: [],
+    error: ''
     }
   }
 
+  componentDidMount() {
+    fetch('https://swapi.co/api/films')
+    .then(response => response.json())
+    .then(data => this.setState({
+      filmData: data.results
+    }))
+    .catch(error => this.setState({ error }))
+
+    fetch('https://swapi.co/api/films')
+    .then(response => response.json())
+    .then(data => this.setState({
+      peopleData: data.results
+    }))
+    .catch(error => this.setState({ error }))
+
+    fetch('https://swapi.co/api/films')
+    .then(response => response.json())
+    .then(data => this.setState({
+      planetData: data.results
+    }))
+    .catch(error => this.setState({ error }))
+
+    fetch('https://swapi.co/api/films')
+    .then(response => response.json())
+    .then(data => this.setState({
+      vehicleData: data.results
+    }))
+    .catch(error => this.setState({ error }))
+  }
 
   render () {
     return (
-
-
-
+      <main>
+        <Crawl movieInfo={this.state} />
+        <title>
+          <h1>SWAPI-BOX</h1>
+          <button className='favorites' img="">Favs <span className="favsCount" >0</span>
+          </button>
+        </title>
+        <nav>
+          <button>People</button>
+          <button>Planets</button>
+          <button>Vehicles</button>
+        </nav>
+        <section className="card-container">
+          <CardContainer />
+        </section>
+      </main>
       )
-
-  }
-
-    
+  }  
 }
 
 export default App;
---
+++ src/Crawl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Crawl from 'react-star-wars-crawl';
+import 'react-star-wars-crawl/lib/index.css';
+
+const Crawl = () => {
+  return (
+    <h2>Hello from Crawl</h2>
+  )
+}
+
+export default Crawl;
