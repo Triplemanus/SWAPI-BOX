c6f0cb05fc56af3d740650646b0dcdb1b633e71d
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-21 09:04:13 -0600

    add styling or card container

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Card from '../Card/Card'
+import './CardContainer.scss'
 
 const CardContainer = ({ peopleData }) => {
   const person = peopleData.map(person => {
     return <Card name={person.name} species={person.species} />
   })
   return (
-    person
+    <section className='card-container'>
+      {person}
+    </section>
   )
 }
 
