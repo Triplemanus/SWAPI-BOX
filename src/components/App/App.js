522dc5b533a9f372341bd88adc25e387ce624fc9
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-21 09:04:33 -0600

    add background image for app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import OpeningCrawl from '../OpeningCrawl/OpeningCrawl';
 import CardContainer from '../CardContainer/CardContainer';
 
