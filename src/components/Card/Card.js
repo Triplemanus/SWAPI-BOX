d954916452186c7dd2702f7e504261235fae0fd5
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-21 08:50:47 -0600

    add scss and card some card styling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Card.scss';
 
 const Card = ({ name, species, homeworld}) => {
   return (
-    <article>
-      <h3>{name}</h3>
-      <button>Favorite me!</button>
-      <p>Homeworld: Tatooine</p>
-      <p>Species: {species}</p>
-      <p>Population: 100000</p>
+    <article className='card'>
+      <div className='card_content'>
+        <h3>{name}</h3>
+        <button>Favorite me!</button>
+        <p>Homeworld: Tatooine</p>
+        <p>Species: {species}</p>
+        <p>Population: 100000</p>
+      </div>
     </article>
   )
 }
--- src/components/OpeningCrawl/OpeningCrawl.js
+++ src/components/OpeningCrawl/OpeningCrawl.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Crawl from 'react-star-wars-crawl';
 import 'react-star-wars-crawl/lib/index.css';
-import './OpeningCrawl.css'
+import './OpeningCrawl.scss'
 const OpeningCrawl = ({title, date, episode, text}) => {
-  console.log(title)
+
   return (
     <article>
-
       <Crawl 
       title = {title}
       subtitle ={episode}
