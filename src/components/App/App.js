e0f6ca1496bed4c04d362092f2e02f7c53103993
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-22 09:59:48 -0600

    refactor fetch to be able to fetch all data needed for people

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   }
 
   componentDidMount() { 
-    const number =  Math.floor(Math.random() * (6 - 2 + 1)) + 1
+    // const number =  Math.floor(Math.random() * (6 - 2 + 1)) + 1
     // fetch('https://swapi.co/api/films')
     // .then(response => response.json())
     // .then(data => this.setState({
@@ -34,59 +34,92 @@ class App extends Component {
     // }))
     // .catch(error => this.setState({ error }))
 
-    // fetch('https://swapi.co/api/people')
-    // .then(response => response.json())
-    // .then(data => this.setState({
-    //   peopleData: data.results.map(person => {
-        // const clean = [
-          // person.name,
-          // Homeworld: person.homeworld,
-          // Species: person.species,
-          // person.birth_year, 
-          // null ,
-          // person.created
-      // ] 
-      // return clean
-    // })
-    // }))
-    // .catch(error => this.setState({ error }))
+    fetch('https://swapi.co/api/people/')
+     .then(response => response.json())
+     .then(data => this.fetchPeople(data.results))
+     .then(data => this.fetchSpecies(data))
+     .then(data => data.map( datum => {
+        const clean = [
+          datum.name,
+          `Homeworld: ${datum.homeworld}`,
+          `Species: ${datum.species}`,
+          `Population: ${datum.population}`,
+          null,
+          datum.id
+        ]
+        return clean
+     }))
+     .then(peopleData => this.setState({ peopleData }))
+     .catch(error => this.setState({ error }))
 
     // fetch('https://swapi.co/api/planets')
     // .then(response => response.json())
     // .then(data => this.setState({
     //   planetData: data.results.map(planet => {
-        // const clean = [
-          // planet.name,
-          // planet.terrain,
-          // planet.population,
-          // planet.climate, 
-          // planet.residents,
-          // planet.created
-          //  ]
-          // return clean
+    //     const clean = [
+    //       planet.name,
+    //       planet.terrain,
+    //       planet.population,
+    //       planet.climate, 
+    //       planet.residents,
+    //       planet.created
+    //        ]
+    //       return clean
+    //   })
     // }))
     // .catch(error => this.setState({ error }))
 
-    fetch('https://swapi.co/api/vehicles')
-    .then(response => response.json())
-    .then(data => this.setState({
-      vehicleData: data.results.map(vehicle => {
-        const clean = [
-          vehicle.name,
-          `Model: ${vehicle.model}`,
-          `Class: ${vehicle.vehicle_class}`,
-          `Passengers: ${vehicle.passengers}`, 
-          null,
-          vehicle.created
-        ]
-        return clean
-      })
-    }))
-    .catch(error => this.setState({ error }))
+    // fetch('https://swapi.co/api/vehicles')
+    // .then(response => response.json())
+    // .then(data => this.setState({
+    //   vehicleData: data.results.map(vehicle => {
+    //     const clean = [
+    //       vehicle.name,
+    //       `Model: ${vehicle.model}`,
+    //       `Class: ${vehicle.vehicle_class}`,
+    //       `Passengers: ${vehicle.passengers}`, 
+    //       null,
+    //       vehicle.created
+    //     ]
+    //     return clean
+    //   })
+    // }))
+    // .catch(error => this.setState({ error }))
   }
+
+  fetchPeople = (people) => {
+    const promises = people.map(person => {
+      return fetch(person.homeworld)
+        .then(response => response.json())
+        .then(data => ({
+          homeworld: data.name,
+          population: data.population,
+          name: person.name,
+          species: person.species
+         }))
+        .catch(error => console.log(error));
+    });
+    return Promise.all(promises);
+  };
+
+
+ fetchSpecies = (people) => {
+  const promises = people.map(person => {
+    return fetch(person.species)
+      .then(response => response.json())
+      .then(data => ({
+        ...person,
+        id: person.created,
+        species: data.name
+      }))
+      .catch(error => console.log(error));
+  });
+  return Promise.all(promises);
+};
 
   render () {
    const { filmData, peopleData, planetData, vehicleData, favoriteCards, landingPage} = this.state;
+   console.log(peopleData)
     return (
     
       <main>
@@ -116,14 +149,14 @@ class App extends Component {
         </nav>
         <section>
           {/* if people is clicked           */}
-          {/* <CardContainer data={peopleData}/> */}
+          <CardContainer data={peopleData}/>
           
           {/* if planets is clicked */}
           {/* <CardContainer data={planetData}/> */}
 
           
           {/* if vehicles is clicked */}
-          <CardContainer data={vehicleData}/>
+          {/* <CardContainer data={vehicleData}/> */}
 
 
         </section>
