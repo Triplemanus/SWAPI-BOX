939fc7b22be4f52fa9451cf0042ab20d19755f5e
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-27 08:39:12 -0600

    refactor people fetch call

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,11 +1,32 @@
 const fetchCalls = {
 
-  fetchMovies() {
+  fetchMovies: () => {
     fetch('https://swapi.co/api/films')
     .then(response => response.json())
     .then(data => this.setState({
       filmData: data.results[number]
     }))
     .catch(error => this.setState({ error }))
+  },
+
+  fetchPeople: () => {
+    fetch('https://swapi.co/api/people/')
+   .then(response => response.json())
+   .then(data => this.fetchPeople(data.results))
+   .then(data => this.fetchSpecies(data))
+   .then(data => data.map( people => {
+      const cleanData = [
+        people.name,
+        `Homeworld: ${people.homeworld}`,
+        `Species: ${people.species}`,
+        `Population: ${people.population}`,
+        null,
+        people.created,
+        false
+      ]
+      return cleanData
+   }))
+   .then(peopleData => this.setState({ peopleData }))
+   .catch(error => this.setState({ error }))
   }
 }
