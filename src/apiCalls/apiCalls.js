090dcbb9ab8122bbdd9bc04944fefcdd97e11437
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-27 09:23:25 -0600

    refactor some fetch calls to make easier to test

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,71 +1,50 @@
 const fetchCalls = {
-
-  fetchMovies: () => {
-    fetch('https://swapi.co/api/films')
-    .then(response => response.json())
-    .then(data => this.setState({
-      filmData: data.results[number]
-    }))
-    .catch(error => this.setState({ error }))
+  fetchPeople: (people) => {
+    const promises = people.map(person => {
+      return fetch(person.homeworld)
+        .then(response => response.json())
+        .then(data => ({
+          homeworld: data.name,
+          population: data.population,
+          name: person.name,
+          species: person.species
+         }))
+        .catch(error => console.log(error));
+    });
+    return Promise.all(promises);
   },
-
-  fetchPeople: () => {
-    fetch('https://swapi.co/api/people/')
-    .then(response => response.json())
-    .then(data => this.fetchPeople(data.results))
-    .then(data => this.fetchSpecies(data))
-    .then(data => data.map( people => {
-      const cleanData = [
-        people.name,
-        `Homeworld: ${people.homeworld}`,
-        `Species: ${people.species}`,
-        `Population: ${people.population}`,
-        null,
-        people.created,
-        false
-      ]
-      return cleanData
-    }))
-    .then(peopleData => this.setState({ peopleData }))
-    .catch(error => this.setState({ error }))
-  },
-
-  fetchPlanets: () => {
-    fetch('https://swapi.co/api/planets/')
-    .then(response => response.json())
-    .then(data => this.fetchResidents(data.results))
-    .then(data => data.map(planet => {
-        const cleanData = [
-          planet.name,
-          `Terrain: ${planet.terrain}`,
-          `Population: ${planet.population}`,
-          `Climate: ${planet.climate}`,
-          `Residents: ${planet.residents}`,
-             planet.created,
-             false
-        ]
-        return cleanData
-    }))
-    .then(planetData => this.setState({ planetData }))
-    .catch(error => this.setState({ error }))
-  },
-  fetchVechicles: () => {
-    fetch('https://swapi.co/api/vehicles')
-    .then(response => response.json())
-    .then(data => this.setState({
-      vehicleData: data.results.map(vehicle => {
-      const cleanData = [
-        vehicle.name,
-        `Model: ${vehicle.model}`,
-        `Class: ${vehicle.vehicle_class}`,
-        `Passengers: ${vehicle.passengers}`, 
-        null,
-        vehicle.created,
-        false
-      ]
-      return cleanData
-      })
-    }))
-    .catch(error => this.setState({ error }))
+ fetchSpecies: (people) => {
+  const promises = people.map(person => {
+    return fetch(person.species)
+      .then(response => response.json())
+      .then(data => ({
+        ...person,
+        id: person.created,
+        species: data.name
+      }))
+      .catch(error => console.log(error));
+  });
+  return Promise.all(promises);
+}, 
+fetchResidents: (planets) => {
+  const allPlanets = planets.map(planet => {
+    let res = [];
+    planet.residents.map(resident => {
+      return fetch(resident)
+      .then(response => response.json())
+      .then(data => res.push(data.name))
+      .catch(error => this.setState({ error }))
+    });
+    return {
+      name: planet.name,
+      terrain: planet.terrain,
+      population: planet.population,
+      climate: planet.climate,
+      residents: res
+    }
+  });
+  return allPlanets
   }
 }
+
+export default fetchCalls;
