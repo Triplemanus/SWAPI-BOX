01ec3da4304db1334bb19bbfaf44f1e4b3058e44
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-27 08:40:45 -0600

    refactor planet fetch call and fix indentation

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -11,10 +11,10 @@ const fetchCalls = {
 
   fetchPeople: () => {
     fetch('https://swapi.co/api/people/')
-   .then(response => response.json())
-   .then(data => this.fetchPeople(data.results))
-   .then(data => this.fetchSpecies(data))
-   .then(data => data.map( people => {
+    .then(response => response.json())
+    .then(data => this.fetchPeople(data.results))
+    .then(data => this.fetchSpecies(data))
+    .then(data => data.map( people => {
       const cleanData = [
         people.name,
         `Homeworld: ${people.homeworld}`,
@@ -25,8 +25,28 @@ const fetchCalls = {
         false
       ]
       return cleanData
-   }))
-   .then(peopleData => this.setState({ peopleData }))
-   .catch(error => this.setState({ error }))
-  }
+    }))
+    .then(peopleData => this.setState({ peopleData }))
+    .catch(error => this.setState({ error }))
+  },
+
+  fetchPlanets: () => {
+    fetch('https://swapi.co/api/planets/')
+    .then(response => response.json())
+    .then(data => this.fetchResidents(data.results))
+    .then(data => data.map(planet => {
+        const cleanData = [
+          planet.name,
+          `Terrain: ${planet.terrain}`,
+          `Population: ${planet.population}`,
+          `Climate: ${planet.climate}`,
+          `Residents: ${planet.residents}`,
+             planet.created,
+             false
+        ]
+        return cleanData
+    }))
+    .then(planetData => this.setState({ planetData }))
+    .catch(error => this.setState({ error }))
+},
 }
