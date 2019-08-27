9644fc8f4519b955ba5914016a72220c258d796f
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-27 16:41:19 -0600

    add favorite message if no user has no favorites and styling

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -3,12 +3,13 @@ import Card from '../Card/Card'
 import './CardContainer.scss'
 import PropTypes from 'prop-types'
 
-const CardContainer = ({ data, favoriteStatus }) => {
+const CardContainer = ({ data, favoriteCards, favoriteStatus }) => {
   const card = data.map(datum => {
     return <Card datum={datum} key={datum[5]} favoriteStatus={favoriteStatus} />
   });
   return (
     <section className='card-components' >
+      {favoriteCards !== undefined && favoriteCards.length === 0 && <h1 className='favorites-message'>You do not have favorites</h1>}
       {card}
     </section>
   )
@@ -19,5 +20,6 @@ export default CardContainer;
 
 CardContainer.propTypes = {
   data: PropTypes.array.isRequired,
+  favoriteCards: PropTypes.array.isRequired,
   favoriteStatus: PropTypes.func.isRequired
 }
