aa4435ae54bb8d3be9b694d3bab473b2a2a54ee1
Aidan McKay <aidanmckay2000@gmail.com>
2019-09-03 09:20:54 -0600

    Updates

---
+++ precache-manifest.0cc4f503161843feb11b5b47bf7e791b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7579c36c9d3bf0ae25f8785adde9bc0",
+    "url": "/SWAPI-BOX/index.html"
+  },
+  {
+    "revision": "b99bed4515bf99426956",
+    "url": "/SWAPI-BOX/static/css/2.d1f26340.chunk.css"
+  },
+  {
+    "revision": "610e74c9623fa5ead7e8",
+    "url": "/SWAPI-BOX/static/css/main.5a86f889.chunk.css"
+  },
+  {
+    "revision": "b99bed4515bf99426956",
+    "url": "/SWAPI-BOX/static/js/2.8e854a82.chunk.js"
+  },
+  {
+    "revision": "610e74c9623fa5ead7e8",
+    "url": "/SWAPI-BOX/static/js/main.d109df1e.chunk.js"
+  },
+  {
+    "revision": "6c9ce82d2da50d3c1b1e",
+    "url": "/SWAPI-BOX/static/js/runtime~main.be3313e7.js"
+  },
+  {
+    "revision": "3d58be7bd3040039e6da735fe74f32dc",
+    "url": "/SWAPI-BOX/static/media/loading.3d58be7b.gif"
+  },
+  {
+    "revision": "54316d8b36837cb28c697ef6039cda09",
+    "url": "/SWAPI-BOX/static/media/star-active.54316d8b.svg"
+  },
+  {
+    "revision": "d037247693fb3643b14492ec812ce19a",
+    "url": "/SWAPI-BOX/static/media/star.d0372476.svg"
+  }
+]);
